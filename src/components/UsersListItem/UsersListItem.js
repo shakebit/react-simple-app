91f8eb76fed7dacfc0664735d9a096f4c0fa07c4
Marcin Biaasek <marcin@platypus.pl>
2021-05-24 12:16:58 +0200

    userslist component

---
+++ src/components/UsersListItem/UsersListItem.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const UsersListItem = ({ userData: { name, average, attendance = '0%' } }) => (
+  <li>
+    <div>{average}</div>
+    <div>
+      <p>{name}</p>
+      <p>attendance: {attendance}</p>
+    </div>
+    <button> X </button>
+  </li>
+);
+
+UsersListItem.propTypes = {
+  userData: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    average: PropTypes.string.isRequired,
+    attendance: PropTypes.string,
+  }),
+};
+
+export default UsersListItem;
